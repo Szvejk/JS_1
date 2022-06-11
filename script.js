46c36b213dea04fc6cf7034a97def386a8718a91
Sara Szwejkowska <saraszw@op.pl>
2022-06-12 00:12:53 +0200

    created lazy-loading and added autoplayed-delay

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
 
-
 const firstSlider = {
-    navigation:{
-        nextEl: '.swiper-button-next',
-        prevEl:'.swiper-button-prev',
-    },
+    ally: true,
+	autoplay: {
+		delay: 3000,
+	},
+    // freeMode: true,
+    lazy:true,
+    loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
 };
 new Swiper('.swiper-container-first', firstSlider);
